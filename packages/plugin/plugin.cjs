4b866afa190ac50db883dfeaa9488f12bdf6cded
KCM <knightedcodemonkey@gmail.com>
2025-12-13 17:01:32 -0600

    refactor: reexport.

--- packages/plugin/plugin.cjs
+++ packages/plugin/plugin.cjs
@@ -1,1 +1,7 @@
-module.exports = require('./dist/cjs/jsx-ts-plugin.js')
+function create(info) {
+  return require('./dist/cjs/jsx-ts-plugin.js').create(info)
+}
+
+module.exports = create
+module.exports.create = create
+module.exports.default = create
